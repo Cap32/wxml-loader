d0c06d0c57b32232aa894c0284eb54803939e665
3went2 <webb.png@gmail.com>
2017-04-13 18:35:23 +0800

    add testing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import { relative, resolve } from 'path';
 import { Parser } from 'htmlparser2';
 import { isUrlRequest } from 'loader-utils';
+import { readFileSync } from 'fs';
 
 export default function (content) {
 	this.cacheable && this.cacheable();
@@ -19,9 +20,9 @@ export default function (content) {
 	const hasIssuer = _module && _module.issuer;
 	const issuerContext = hasIssuer && _module.issuer.context || context;
 
-	const emit = (dep) => {
+	const emit = (dep, content) => {
 		const filename = relative(issuerContext, dep);
-		this.emitFile(filename, require('fs').readFileSync(dep, 'utf8'));
+		this.emitFile(filename, content || readFileSync(dep, 'utf8'));
 	};
 
 	const logValue = (name, val) => {
@@ -36,7 +37,7 @@ export default function (content) {
 	const parser = new Parser({
 		onattribute: logValue,
 		onend: () => {
-			emit(resourcePath);
+			emit(resourcePath, content);
 			callback(null, '');
 		}
 	});
---
+++ test/index.js
@@ -0,0 +1,27 @@
+
+import loader from '../src';
+import assert from 'assert';
+
+describe('wxml-loader', () => {
+	it('should convert requires', (done) => {
+		const noop = () => {};
+		const cb = (err, result) => {
+			assert.equal(result, '');
+			done();
+		};
+		loader.call(
+			{
+				async: () => cb,
+				emitFile: noop,
+				addDependency: noop,
+				loadModule: noop,
+				options: {
+					context: 'src',
+				},
+				context: 'src',
+				resourcePath: 'src',
+			},
+			'<view></view>',
+		);
+	});
+});
