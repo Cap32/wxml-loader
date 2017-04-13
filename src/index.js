422facead212421a94fd2e4d828af05b64f4d91a
3went2 <webb.png@gmail.com>
2017-04-13 18:06:59 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+
+import { relative, resolve } from 'path';
+import { Parser } from 'htmlparser2';
+
+export default function (content) {
+	this.cacheable && this.cacheable();
+	if (!this.emitFile) {
+		throw new Error('emitFile is required from module system');
+	}
+
+	const callback = this.async();
+	const {
+		options: { context },
+		context: resourceDir,
+		resourcePath,
+		_module,
+	} = this;
+	const hasIssuer = _module && _module.issuer;
+	const issuerContext = hasIssuer && _module.issuer.context || context;
+
+	const emit = (dep) => {
+		const filename = relative(issuerContext, dep);
+		this.emitFile(filename, require('fs').readFileSync(dep, 'utf8'));
+	};
+
+	const logValue = (name, val) => {
+		if (name !== 'src' || /\{/.test(val)) { return; }
+
+		const dep = resolve(resourceDir, val);
+		this.addDependency(dep);
+		this.loadModule(dep, () => {});
+		emit(dep);
+	};
+
+	const parser = new Parser({
+		onattribute: logValue,
+		onend: () => {
+			emit(resourcePath);
+			callback(null, '');
+		}
+	});
+	parser.write(content);
+	parser.end();
+}
