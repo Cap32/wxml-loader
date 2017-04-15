f56fea5025997c69207af889231df3bc9b312be6
Webb Peng <webb.png@gmail.com>
2017-04-16 01:15:14 +0800

    add `root` option

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 
-import { relative, resolve } from 'path';
-import { Parser } from 'htmlparser2';
-import { isUrlRequest } from 'loader-utils';
+import { relative } from 'path';
+import sax from 'sax';
+import { isUrlRequest, urlToRequest, getOptions } from 'loader-utils';
 import { readFileSync } from 'fs';
+
+const isRelativeUrl = (url) => /^\.+\//.test(url);
+const isSrc = (name) => name === 'src';
+const noop = () => {};
+const replaceAt = (str, start, end, replacement) =>
+	str.slice(0, start) + replacement + str.slice(end)
+;
 
 export default function (content) {
 	this.cacheable && this.cacheable();
@@ -17,30 +24,52 @@ export default function (content) {
 		resourcePath,
 		_module,
 	} = this;
+	const options = getOptions(this) || {};
+
 	const hasIssuer = _module && _module.issuer;
 	const issuerContext = hasIssuer && _module.issuer.context || context;
+
+	const {
+		root = issuerContext,
+	} = options;
+
+	const urls = [];
 
 	const emit = (dep, content) => {
 		const filename = relative(issuerContext, dep);
 		this.emitFile(filename, content || readFileSync(dep, 'utf8'));
 	};
 
-	const logValue = (name, val) => {
-		if (name !== 'src' || !isUrlRequest(val)) { return; }
+	const replace = ({ url, startIndex, endIndex }) =>
+		content = replaceAt(content, startIndex, endIndex, url)
+	;
 
-		const dep = resolve(resourceDir, val);
-		this.addDependency(dep);
-		this.loadModule(dep, () => {});
-		emit(dep);
+	const parser = sax.parser(false, { lowercase: true });
+
+	parser.onattribute = ({ name, value = '' }) => {
+		if (!isSrc(name) || !isUrlRequest(value, root)) { return; }
+
+		let url;
+
+		if (isRelativeUrl(value)) {
+			url = urlToRequest(value, resourceDir);
+		}
+		else {
+			const endIndex = parser.position - 1;
+			const startIndex = endIndex - value.length;
+			const moduleUrl = urlToRequest(value, root);
+			url = relative(resourceDir, moduleUrl);
+			urls.unshift({ url, startIndex, endIndex });
+		}
+
+		this.loadModule(url, noop);
 	};
 
-	const parser = new Parser({
-		onattribute: logValue,
-		onend: () => {
-			emit(resourcePath, content);
-			callback(null, '');
-		}
-	});
-	parser.write(content);
-	parser.end();
+	parser.onend = () => {
+		urls.forEach(replace);
+		emit(resourcePath, content);
+		callback(null, '');
+	};
+
+	parser.write(content).close();
 }
