b66c43d4705888b5792b537fe7977642add09616
3went2 <webb.png@gmail.com>
2017-04-17 18:41:02 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 
 import { resolve } from 'path';
-import { resolve as urlResolve } from 'url';
 import sax from 'sax';
 import { Script } from 'vm';
 import Minimize from 'minimize';
 import { isUrlRequest, urlToRequest, getOptions } from 'loader-utils';
 
+const ROOT_TAG_NAME = 'xxx-wxml-root-xxx';
+const ROOT_TAG_START = `<${ROOT_TAG_NAME}>`;
+const ROOT_TAG_END = `</${ROOT_TAG_NAME}>`;
+const ROOT_TAG_LENGTH = ROOT_TAG_START.length;
+
 const isSrc = (name) => name === 'src';
-const isRelative = (url) => /^\.+\//.test(url);
 const replaceAt = (str, start, end, replacement) =>
 	str.slice(0, start) + replacement + str.slice(end)
 ;
 
-const extract = (src) => {
+const extract = (src, __webpack_public_path__) => {
 	const script = new Script(src, { displayErrors: true });
 	const sandbox = {
-		__webpack_public_path__: '',
+		__webpack_public_path__,
 		module: {},
 	};
 	script.runInNewContext(sandbox);
@@ -34,7 +37,6 @@ export default function (content) {
 	const callback = this.async();
 	const {
 		options: { context, output },
-		// context: resourceDir,
 		_module,
 	} = this;
 	const options = getOptions(this) || {};
@@ -44,7 +46,7 @@ export default function (content) {
 
 	const {
 		root = resolve(context, issuerContext),
-		publicPath = output.publicPath || '/',
+		publicPath = output.publicPath || '',
 		minimize: forceMinimize,
 		...minimizeOptions,
 	} = options;
@@ -58,9 +60,6 @@ export default function (content) {
 	;
 
 	const loadModule = (request) => new Promise((resolve, reject) => {
-		// if (isRelative(request)) {
-		// 	request = join(resourceDir, request);
-		// }
 		this.addDependency(request);
 		this.loadModule(request, (err, src) => {
 			if (err) { reject(err); }
@@ -68,11 +67,11 @@ export default function (content) {
 		});
 	});
 
+	const xmlContent = `${ROOT_TAG_START}${content}${ROOT_TAG_END}`;
+
 	const replace = async ({ request, startIndex, endIndex }) => {
 		const src = await loadModule(request);
-		const url = extract(src);
-		const replacement = isRelative(url) ? url : publicPath + url;
-		// console.log('url', url, publicPath);
+		const replacement = extract(src, publicPath);
 		content = replaceAt(content, startIndex, endIndex, replacement);
 	};
 
@@ -81,7 +80,7 @@ export default function (content) {
 	parser.onattribute = ({ name, value = '' }) => {
 		if (!isSrc(name) || !isUrlRequest(value, root)) { return; }
 
-		const endIndex = parser.position - 1;
+		const endIndex = parser.position - 1 - ROOT_TAG_LENGTH;
 		const startIndex = endIndex - value.length;
 		const request = urlToRequest(value, root);
 		requests.unshift({ request, startIndex, endIndex });
@@ -89,7 +88,8 @@ export default function (content) {
 
 	parser.onend = async () => {
 		try {
-			await Promise.all(requests.map(replace));
+			for (const req of requests) { await replace(req); }
+
 			if (shouldMinimize) { content = minimize(content); }
 			callback(null, content);
 		}
@@ -98,5 +98,5 @@ export default function (content) {
 		}
 	};
 
-	parser.write(content).close();
+	parser.write(xmlContent).close();
 }
